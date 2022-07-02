17b8be115eb2e3dd1b8a349e97cb073a077584dc
Akshay <deadshot9987@gmail.com>
2022-07-02 16:56:00 +0530

    created server using express

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'Adidas-slim',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality Shirt',
+    },
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-Pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality Pant',
+    },
+  ],
+};
+
+export default data;
